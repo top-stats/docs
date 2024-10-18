233d08f753b451016e88d42152c40623e5aed572
676943 <Xignotic84@gmail.com>
2024-10-18 20:44:18 +0200

    Add bots documentation and basic settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,21 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://docs.topstats.gg',
 	integrations: [
 		starlight({
-			title: 'Docs | TopStats.gg',
+			title: 'TopStats.gg',
 			social: {
-				github: 'https://github.com/top-stats/web',
+				github: 'https://github.com/top-stats',
 				discord: 'https://discord.com/invite/jjEauJXuZc'
 			},
+			logo: {
+				src: './src/assets/favicon.svg',
+			},
+			editLink: {
+				baseUrl: 'https://github.com/top-stats/docs/edit/master/',
+			},
+			favicon: './src/assets/favicon.svg',
 			sidebar: [
 				{
 					label: 'Guides',
