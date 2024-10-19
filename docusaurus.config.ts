35f75c6c65e783b416396e4d653179af7ffb6f1c
676943 <Xignotic84@gmail.com>
2024-10-19 13:14:00 +0200

    chore: update blog link for new blog

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -91,7 +91,7 @@ const config: Config = {
           label: 'Docs',
         },
         {
-          to: '/blog',
+          to: 'https://blog.topstats.gg',
           label: 'Blog',
           position: 'left'
         },
