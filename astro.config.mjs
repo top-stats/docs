4068622b91c618c09868b1e1b60797ec99b2a8d0
Luke Stoodley <luke@ichigo.uk>
2024-10-19 21:33:35 +0200

    update nginx and astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from '@astrojs/starlight'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://docs.topstats.gg',
+  output: 'static',
   integrations: [
     starlight({
       title: 'TopStats',
