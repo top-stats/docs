60c4c9ff4252521cda68fa158dadb508dddc646d
676943 <Xignotic84@gmail.com>
2024-10-24 13:27:10 +0200

    chore: update discord link to direct through topstats first

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -67,7 +67,7 @@ const config: Config = {
   themeConfig: {
     announcementBar: {
       id: `announcementBar`,
-      content: `🎉️ <b>Something new is brewing... <a target="_blank" href="https://discord.com/invite/jjEauJXuZc">Join Our Discord</a> to find out more!</b> 🥳️`,
+      content: `🎉️ <b>Something new is brewing... <a target="_blank" href="https://topstats.gg/discord?ref=legacyDocs">Join Our Discord</a> to find out more!</b> 🥳️`,
     },
     metadata: [
       {
