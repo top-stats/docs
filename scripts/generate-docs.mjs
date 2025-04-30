36c264673243a99fb090d5245fa06339504f8b4e
xignotic84 <Xignotic84@gmail.com>
2025-04-30 14:00:17 +0200

    chore: add api routes

--- scripts/generate-docs.mjs
+++ scripts/generate-docs.mjs
@@ -1,9 +1,11 @@
-import { generateFiles } from 'fumadocs-openapi';
+// Pages are no longer automatically generated, instead pages are manually grouped due to limitations with grouping with FumaDocs and OpenAPI Schema.
+// Open API Schema is still used see referenced API pages.
 
+/*
 void generateFiles({
-  input: ['./openapi.json'], // the OpenAPI schemas
-  output: './content/docs',
-  // we recommend to enable it
-  // make sure your endpoint description doesn't break MDX syntax.
-  includeDescription: true,
+  input: ['./openapi.json'], // Path to your OpenAPI schema
+  output: './content/docs/reference', // Output folder for docs
+  includeDescription: true, // Include descriptions for each endpoint
+  groupBy: 'tag',
 });
+ */
