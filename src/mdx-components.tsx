9749a0a1f67930ef76696e92fd08b332d640ed9b
xignotic84 <Xignotic84@gmail.com>
2025-05-01 13:17:47 +0200

    chore: some fixes

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,7 @@
-import defaultComponents from 'fumadocs-ui/mdx';
-import type { MDXComponents } from 'mdx/types';
-import { APIPage } from 'fumadocs-openapi/ui';
-import { openapi } from '@/lib/source';
+import defaultComponents from 'fumadocs-ui/mdx'
+import type { MDXComponents } from 'mdx/types'
+import { APIPage } from 'fumadocs-openapi/ui'
+import { openapi } from '@/lib/source'
 
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
@@ -9,5 +9,5 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     ...defaultComponents,
     APIPage: (props) => <APIPage {...openapi.getAPIPageProps(props)} />,
     ...components,
-  };
+  }
 }
