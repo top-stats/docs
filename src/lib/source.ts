1daabcfd8b6407064149a830ba02941cc5308211
xignotic84 <Xignotic84@gmail.com>
2025-05-01 12:09:12 +0200

    chore: layout changes

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,6 +1,8 @@
 import { docs } from '@/.source'
 import { loader } from 'fumadocs-core/source'
 import { attachFile, createOpenAPI } from 'fumadocs-openapi/server'
+import { icons } from 'lucide-react';
+import { createElement } from 'react';
 
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
@@ -11,6 +13,14 @@ export const source = loader({
   },
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
+  icon(icon) {
+    if (!icon) {
+      // You may set a default icon
+      return;
+    }
+
+    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+  },
 })
 
 export const openapi = createOpenAPI({})
