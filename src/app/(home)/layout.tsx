397e1ce509c070f2f7abcfac21b817864683bcf5
xignotic84 <Xignotic84@gmail.com>
2025-05-04 16:31:48 +0200

    fix: build

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,9 +2,8 @@ import type { ReactNode } from 'react'
 import { HomeLayout } from 'fumadocs-ui/layouts/home'
 import { baseOptions } from '@/app/layout.config'
 import { BookIcon } from 'lucide-react'
-import { Metadata } from 'next'
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Docs | TopStats.gg',
   description: 'Documentation and Guides for TopStats.gg',
 }
