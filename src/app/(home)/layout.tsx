08088d47cdaebedac37129d03ad178e4228d077d
12LuA <71656192+12LuA@users.noreply.github.com>
2025-07-23 13:26:56 +0200

    update link text to 'API-Documentation' in layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -15,7 +15,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     links={[
       {
         icon: <BookIcon />,
-        text: 'Docs',
+        text: 'API-Documentation',
         url: '/docs',
       },
     ]}
