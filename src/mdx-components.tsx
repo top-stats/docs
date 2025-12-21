ed94bc4d9e8ae5152b2b0f059274f6aa8b83da5e
Franz Herrmann <Xignotic84@gmail.com>
2025-12-21 15:25:51 +0100

    fix: build

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,4 +1,4 @@
-import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
+import { ImageZoom } from 'fumadocs-ui/components/image-zoom'
 import defaultMdxComponents from 'fumadocs-ui/mdx'
 import { APIPage } from '@/components/api-page'
 import type { MDXComponents } from 'mdx/types'
