783d6c59ee209571f5bc8596e5639d0c187fcb0e
12LuA <71656192+12LuA@users.noreply.github.com>
2025-12-21 20:51:00 +0100

    feat: add Servers link to layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react'
 import { HomeLayout } from 'fumadocs-ui/layouts/home'
 import { baseOptions } from '@/app/layout.config'
-import { BookIcon, BotIcon } from 'lucide-react'
+import { BookIcon, BotIcon, ServerIcon } from 'lucide-react'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -23,6 +23,11 @@ export default function Layout({ children }: { children: ReactNode }) {
           icon: <BookIcon />,
           text: 'API',
           url: '/docs/api',
+        },
+        {
+          icon: <ServerIcon />,
+          text: 'Servers',
+          url: '/docs/servers',
         },
       ]}
     >
