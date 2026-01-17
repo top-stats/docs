0bb6fb8deb7c45e72f851126ea6c0d9872f7b7f0
12LuA <71656192+12LuA@users.noreply.github.com>
2026-01-17 19:05:26 +0100

    fix: adjust button height in NotFound component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,7 +10,7 @@ export default function NotFound() {
 
       <a
         href='/'
-        className='mt-6 inline-flex h-[42px] items-center justify-center gap-2 rounded-[10px] border border-gray-200 bg-white px-4 py-2 shadow-sm transition-all duration-300 hover:scale-105 hover:border-[#8A9DE4] hover:bg-accent hover:text-gray-900 hover:shadow-md dark:border-[#ffffff1a] dark:bg-[#10111a] dark:hover:text-white group'
+        className='mt-6 inline-flex h-10.5 items-center justify-center gap-2 rounded-[10px] border border-gray-200 bg-white px-4 py-2 shadow-sm transition-all duration-300 hover:scale-105 hover:border-[#8A9DE4] hover:bg-accent hover:text-gray-900 hover:shadow-md dark:border-[#ffffff1a] dark:bg-[#10111a] dark:hover:text-white group'
       >
         <span className='text-base font-medium'>Return Home</span>
         <ArrowRightIcon className='h-4 w-4 transition-transform group-hover:translate-x-1' />
