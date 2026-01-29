c920f852c47ed9176f7e8a4c8f6060ceb445946d
12LuA <71656192+12LuA@users.noreply.github.com>
2026-01-29 19:45:14 +0100

    fix: update 404 page styles and structure

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,18 @@
-import { ArrowRightIcon } from 'lucide-react'
+import { cn } from '@/lib/cn'
+import { buttonVariants } from 'fumadocs-ui/components/ui/button'
 
 export default function NotFound() {
   return (
     <div className='flex min-h-screen flex-col items-center justify-center text-center'>
       <h1 className='text-9xl font-bold'>404</h1>
-      <p className='text-muted-foreground/80 scroll-m-20'>
+      <p className='text-fd-muted-foreground scroll-m-20'>
         Uh oh! We couldn't find what you were looking for!
       </p>
-
       <a
         href='/'
-        className='mt-6 inline-flex h-10.5 items-center justify-center gap-2 rounded-[10px] border border-gray-200 bg-white px-4 py-2 shadow-sm transition-all duration-300 hover:scale-105 hover:border-[#8A9DE4] hover:bg-accent hover:text-gray-900 hover:shadow-md dark:border-[#ffffff1a] dark:bg-[#10111a] dark:hover:text-white group'
+        className={cn(buttonVariants({ variant: 'primary' }), 'mt-6')}
       >
-        <span className='text-base font-medium'>Return Home</span>
-        <ArrowRightIcon className='h-4 w-4 transition-transform group-hover:translate-x-1' />
+        Return Home
       </a>
     </div>
   )
